b2a3899f5b9925937cbee89081553f10b39d8c67
nagarium <nagardinal@gmail.com>
2023-03-28 18:47:29 +0200

    fixed zoom state so app opens for me

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import arduino as a
 root = tk.Tk()
 
 root.title("Essential Tremor Analysis")
-root.wm_attributes('-zoomed', True)
+root.state('zoomed')
 
 canvas1 = tk.Canvas(root)
 canvas1.pack(fill=tk.BOTH, expand=True)
