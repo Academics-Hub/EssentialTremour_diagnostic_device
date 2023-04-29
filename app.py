5f4934a51da2dbfb2844a2891c015911b179b6da
JonoLF <jono.faller@gmail.com>
2023-04-29 14:11:18 +0200

    major improvements to graphical aspects, noise filtered out and offset of input signal accounted for on resting input. Needs further testing

--- app.py
+++ app.py
@@ -33,28 +33,43 @@ def getCSV():
 
     # Display a success message after importing the CSV file
     messagebox.showinfo("Success", "CSV file imported successfully")
-    plotData()
+    plotData(10)
 
 # Add a command to the "File" menu to import a CSV file
 file_menu.add_command(label="Import CSV File and plot data", command=getCSV)
 
 
 def applyFourierTransform():
-    # Get the time and signal data from the DataFrame
+    # Get the signal data from the DataFrame
     signal = df.iloc[:, 0]
+    #first account for noise and shift up by 55
+    for i in range(len(signal)):
+        signal[i] = signal[i] + 55
 
     # Apply the Fourier Transform to the signal data
     fourier_transform = np.fft.fft(signal)
 
+    # filter out the massive noise spike at 0Hz
+    fourier_transform = np.delete(fourier_transform, 0)
     # Shift the zero-frequency component to the center of the spectrum
     fourier_transform_shifted = np.fft.fftshift(fourier_transform)
 
     # Calculate the frequencies for each element in the Fourier Transform
-    frequencies = np.fft.fftfreq(fourier_transform_shifted.size)
+    
+    frequencies = np.fft.fftfreq(fourier_transform_shifted.size, d=0.1)
 
-    return np.fft.fftshift(frequencies), fourier_transform_shifted
+    return np.fft.fftshift(frequencies), normaliseSpectrum(fourier_transform_shifted)
 
-def plotData():
+def normaliseSpectrum(fourier_transform: np.ndarray) -> np.ndarray:
+    #find the 90% percentile of the data
+    percentile = np.percentile(fourier_transform, 90)
+    #if value above percentile keep it, if not, discard it
+    for i in range(len(fourier_transform)):
+        if fourier_transform[i] < percentile:
+            fourier_transform[i] = 0
+    return fourier_transform
+
+def plotData(time):
     frequencies, fourier_transform = applyFourierTransform()
     figure1 = plt.Figure(figsize=(5,5), dpi=100)
     figure2 = plt.Figure(figsize=(5,5), dpi=100)
@@ -63,16 +78,19 @@ def plotData():
 
     # plot original signal
     ax1.plot(np.arange(len(df.iloc[:, 0])), df.iloc[:, 0])
-    ax1.set_title('original signal')
-    ax1.set_xlabel('time (10s)')
+    ax1.set_title('Recorded change in acceleration')
+    ax1.set_xlabel('time (' + str(time) + 's)')
+    ax1.set_ylabel('Amplitude (m/s^2)')
     ax1.set_xticks([])
     linePlot1 = FigureCanvasTkAgg(figure1, root)
-    linePlot1.get_tk_widget().place(relx=0.3, rely=0.3, anchor=tk.CENTER, relwidth=0.4, relheight=0.5)
+    linePlot1.get_tk_widget().place(relx=0.3, rely=0.3, anchor=tk.CENTER, relwidth=0.5, relheight=0.6)
     # Plot the real part of the Fourier transform
     ax2.plot(frequencies, abs(fourier_transform))
-    ax2.set_title('frequency spectra')
+    ax2.set_title('Normalized frequency spectrum, \ndisplaying spikes in the \n90th percentile of frequencies')
+    ax2.set_xlabel('frequency (Hz)')
+    ax2.set_yticks([])
     linePlot2 = FigureCanvasTkAgg(figure2, root)
-    linePlot2.get_tk_widget().place(relx=0.7, rely=0.3, anchor=tk.CENTER, relwidth=0.4, relheight=0.5)
+    linePlot2.get_tk_widget().place(relx=0.7, rely=0.3, anchor=tk.CENTER, relwidth=0.5, relheight=0.6)
 
 file_menu.add_command(label="Plot data", command=plotData)
 
