ed9b8d6ac72eb0c172f627cc8709ff1ea3c80081
JonoLF <jono.faller@gmail.com>
2023-04-30 13:14:55 +0200

    file menu -> buttons

--- app.py
+++ app.py
@@ -8,22 +8,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import recordPatient
 
-
 root = tk.Tk()
-
 root.title("Essential Tremor Analysis")
-#root.state('zoomed')
-
-canvas1 = tk.Canvas(root)
-canvas1.pack(fill=tk.BOTH, expand=True)
-
-# Create a menu bar and add it to the root window
-menu_bar = tk.Menu(root)
-root.config(menu=menu_bar)
-
-# Create a "File" menu and add it to the menu bar
-file_menu = tk.Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(label="Menu", menu=file_menu)
 
 def getCSV():
     global df
@@ -33,9 +19,18 @@ def getCSV():
     # Display a success message after importing the CSV file
     messagebox.showinfo("Success", "CSV file imported successfully")
     plotData(10)
+    
+def patientRecordOnClick():
+    patient = recordPatient.patient("test")
+    patient.recordPatient(10)
 
 # Add a command to the "File" menu to import a CSV file
-file_menu.add_command(label="Import CSV File and plot data", command=getCSV)
+buttonFrame = tk.Frame(root)
+buttonFrame.pack(side=tk.TOP, pady=10, padx=10)
+importButton = tk.Button(buttonFrame, text="Import CSV File and plot data", command=getCSV)
+importButton.pack(side=tk.LEFT, pady=10, padx=10)
+plotButton = tk.Button(buttonFrame, text="Record patient data", command=patientRecordOnClick)
+plotButton.pack(side=tk.LEFT, pady=10, padx=10)
 
 
 def applyFourierTransform():
@@ -68,6 +63,9 @@ def normaliseSpectrum(fourier_transform: np.ndarray) -> np.ndarray:
             fourier_transform[i] = 0
     return fourier_transform
 
+canvas = tk.Canvas(root)
+canvas.pack(anchor=tk.S, expand=True, side=tk.BOTTOM, ipadx=10, ipady=10, fill=tk.BOTH)
+
 def plotData(time):
     frequencies, fourier_transform = applyFourierTransform()
     figure1 = plt.Figure(figsize=(5,5), dpi=100)
@@ -81,23 +79,14 @@ def plotData(time):
     ax1.set_xlabel('time (' + str(time) + 's)')
     ax1.set_ylabel('Amplitude (m/s^2)')
     ax1.set_xticks([])
-    linePlot1 = FigureCanvasTkAgg(figure1, root)
-    linePlot1.get_tk_widget().place(relx=0.3, rely=0.3, anchor=tk.CENTER, relwidth=0.5, relheight=0.6)
+    linePlot1 = FigureCanvasTkAgg(figure1, canvas)
+    linePlot1.get_tk_widget().place(relx=0.3, rely=0.5, anchor=tk.CENTER, relwidth=0.5, relheight=0.6)
     # Plot the real part of the Fourier transform
     ax2.plot(frequencies, abs(fourier_transform))
     ax2.set_title('Normalized frequency spectrum, \ndisplaying spikes in the \n90th percentile of frequencies')
     ax2.set_xlabel('frequency (Hz)')
     ax2.set_yticks([])
-    linePlot2 = FigureCanvasTkAgg(figure2, root)
-    linePlot2.get_tk_widget().place(relx=0.7, rely=0.3, anchor=tk.CENTER, relwidth=0.5, relheight=0.6)
-
-file_menu.add_command(label="Plot data", command=plotData)
-
-def patientRecordOnClick():
-    patient = recordPatient.patient("test")
-    patient.recordPatient(10)
-    
-
-file_menu.add_command(label="Record patient data", command=patientRecordOnClick)
+    linePlot2 = FigureCanvasTkAgg(figure2, canvas)
+    linePlot2.get_tk_widget().place(relx=0.7, rely=0.5, anchor=tk.CENTER, relwidth=0.5, relheight=0.6)
 
 root.mainloop()
