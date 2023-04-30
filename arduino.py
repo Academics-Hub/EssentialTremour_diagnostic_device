24f2724b243ecfa35255bbab534ebe0f00173280
JonoLF <jono.faller@gmail.com>
2023-04-30 16:30:45 +0200

    seperated out codebase into OOP principles

--- arduino.py
+++ arduino.py
@@ -1,22 +1,16 @@
 import serial
 from datetime import datetime
 import csv
-import tkinter as tk
-from tkinter import messagebox
-import numpy as np
-import time
-class arduino:
+import tkinter
+class Arduino:
     def __init__(self, port, baudrate):
         self.port = port
         self.baudrate = baudrate
     
     def read(self, filename, timer):
         ser = serial.Serial(self.port, self.baudrate)
-        #print(t1)
-
         self.timer = timer
-        messagebox.showinfo("Please wait", "Reading patient data for " + str(timer) + " seconds")
-
+        tkinter.messagebox.showinfo("Please wait", "Reading patient data for " + str(timer) + " seconds")
         t1 = datetime.now()
         data = []
         with open(filename, 'w') as f:
@@ -26,9 +20,7 @@ class arduino:
                     break
                 temp = ser.readline().decode().strip()
                 data.append(int(temp))
-            
             for i in range(len(data)):
                 writer.writerow([data[i]])
-
         ser.close()
     
