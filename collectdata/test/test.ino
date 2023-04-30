6aec5e2ba0b00173886ab646da8808b5c9e423cd
nagarium <nagardinal@gmail.com>
2023-04-30 15:26:21 +0200

    data

--- collectdata/test/test.ino
+++ collectdata/test/test.ino
@@ -1,35 +1,5 @@
 #include <Nano33BLE_System.h>
-
 #include <SPI.h>
-
-/************************************************************************
-
-  Test of Pmod ACL2
-
-*************************************************************************
-
-  Description: Pmod_ACL2
-  The 3 components X, Y, and Z of the acceleration, and the temperature
-  are displayed in the serial monitor.
-
-  Material
-  1. Arduino Uno
-  2. Pmod ACL2 (dowload library https://github.com/annem/ADXL362 )
-  3. Adafruit TXB0108
-
-  Wiring
-  Module<----------> Arduino
-  VCC     to        3V3
-  GND     to        GND
-  SCK     to        13 (SCK)
-  MOSI    to        12 (MOSI)
-  MISO    to        11 (MIS0)
-  CS      to        10
-    time.sleep(0.001)
-  a logic level converter must be used!!!
-
-************************************************************************/
-
 #define CS 10 //chip select pin
 
 // Call of libraries
