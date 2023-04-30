9389973055f63c24184ae34bf8e210aee5d96995
JonoLF <jono.faller@gmail.com>
2023-04-30 16:55:27 +0200

    .

--- app.py
+++ app.py
@@ -11,23 +11,22 @@ def getCSV():
     import_file_path = tkinter.filedialog.askopenfilename()
     data = pandas.read_csv(import_file_path)
     tkinter.messagebox.showinfo("Success", "CSV file imported successfully")
-    patientPlot = plot.Plot(canvas, data)
+    patientPlot = plot.Plot(plot_canvas, data)
     patientPlot.createPatientPlot()
 
 def patientRecordOnClick():
     patient = recordPatient.patient("test")
     patient.recordPatient()
-    patientPlot = plot.Plot(canvas, data)
+    patientPlot = plot.Plot(plot_canvas, data)
     patientPlot.createPatientPlot()
-
+#buttons to carry out functionality
 buttonFrame = tkinter.Frame(root)
 buttonFrame.pack(side=tkinter.TOP, pady=10, padx=10)
 importButton = tkinter.Button(buttonFrame, text="Import CSV File and plot data", command=getCSV)
 importButton.pack(side=tkinter.LEFT, pady=10, padx=10)
 plotButton = tkinter.Button(buttonFrame, text="Record patient data", command=patientRecordOnClick)
 plotButton.pack(side=tkinter.LEFT, pady=10, padx=10)
-
-canvas = tkinter.Canvas(root)
-canvas.pack(anchor=tkinter.S, expand=True, side=tkinter.BOTTOM, ipadx=10, ipady=10, fill=tkinter.BOTH)
-
+#plot canvas
+plot_canvas = tkinter.Canvas(root)
+plot_canvas.pack(anchor=tkinter.S, expand=True, side=tkinter.TOP, padx=5, pady=5, fill=tkinter.BOTH )
 root.mainloop()
