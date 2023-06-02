9d0b77de7434bf510b061d2091f3b47d61261aae
JonoLF <jono.faller@gmail.com>
2023-06-02 17:06:59 +0200

    .

--- signalProcessing.py
+++ signalProcessing.py
@@ -8,7 +8,7 @@ class Signal:
         self.data = data
 
     def _antialisingFilter(self) -> pd.DataFrame:
-        filter_band = signal.cheby1(12, 1, [3.8, 100], 'bp', fs=200, output='sos')
+        filter_band = signal.cheby1(12, 1, [3.8, 100], 'bp', fs=250, output='sos')
         filteredSignal = signal.sosfilt(filter_band, self.data)
         return pd.DataFrame(filteredSignal)
         
